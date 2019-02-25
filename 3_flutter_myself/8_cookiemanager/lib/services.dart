9817aaf88d55895a0406c398a19a30c6718d8f96
SilenceZhou <1063123603@qq.com>
2019-02-25 23:55:37 +0800

    cookie处理

---
+++ 3_flutter_myself/8_cookiemanager/lib/services.dart
@@ -0,0 +1,29 @@
+const njqheaders = {
+  'Accept': 'application/json',
+  "useVersion": "3.1.0",
+  "isEncoded": "1",
+  "bundleId": "com.nongfadai.iospro",
+  "loginSource": "IOS",
+  "Content-Type": "application/json",
+  "sourceVersion": "iphonexmax" // 必须要有登陆源才可以登陆，为什么？
+};
+
+/// 请求头
+const kCookieHeader = "136a3d03-9748-4f83-a54f-9b2a93f979a0";
+
+const kMYSESSIONID = "MYSESSIONID=";
+
+/// base
+const njqbaseUrl = "https://appbg.lcfarm.com";
+
+const homeNoviceListUrl = "/api/product/list/noviceProductList.htm";
+
+/// regularList ()
+/// 参数: currentPage=1
+const homeRegularListUrl = "/api/product/list/regularProductList.htm";
+
+/// AD
+/// params: adType:homeAd (homeBanner,homeAd,homeBottom)
+const adUrl = "/api/mediaAnnouncement/getAdInfo.htm";
+
+const loginUrl = "https://appbg.lcfarm.com/api/user/login.htm";
