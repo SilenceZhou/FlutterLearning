e340000e1fb592733e2d0ba3cc2d7d74d957aeec
SilenceZhou <1063123603@qq.com>
2019-03-11 23:23:08 +0800

    bloc登陆

--- 3_flutter_myself/12_bloc_demo/bloc_login_page/lib/authenticationBloc/Authentication_bloc.dart
+++ 3_flutter_myself/12_bloc_demo/bloc_login_page/lib/authenticationBloc/Authentication_bloc.dart
@@ -7,6 +7,7 @@ import 'package:meta/meta.dart';
 class AuthenticationBloc
     extends Bloc<AuthenticationEvent, AuthenticationState> {
   final UserRepository userRepository;
+
   AuthenticationBloc({@required this.userRepository})
       : assert(userRepository != null);
 
