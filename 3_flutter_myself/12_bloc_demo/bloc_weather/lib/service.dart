c626639541f8bad72d29b52f62a56a5121ca4b40
SilenceZhou <1063123603@qq.com>
2019-03-12 17:39:36 +0800

    bloc_weather初始化

---
+++ 3_flutter_myself/12_bloc_demo/bloc_weather/lib/service.dart
@@ -0,0 +1,5 @@
+const baseUrl = "https://www.metaweather.com/api";
+const api = "/api/location/search/";
+
+// /api/location/search/?query=$city to get a locationId for a given city name
+// /api/location/$locationId to get the weather for a given locationId
