94003dfb196d2df0a1285de9f3fec292557fd3c8
SilenceZhou <1063123603@qq.com>
2019-04-17 14:33:34 +0800

    修改

---
+++ 3_flutter_myself/16_counterDemo/2_counter_third_flutterbloc2/lib/bloc/counter_event.dart
@@ -0,0 +1,17 @@
+import 'package:meta/meta.dart';
+
+// @immutable
+// abstract class CounterEvent {}
+
+// class IncrementEvent extends CounterEvent {
+  
+// }
+
+// class DecrementEvent extends CounterEvent {
+  
+// }
+
+
+enum CounterEvent{increment, decrement}
+
+
