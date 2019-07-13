99b3339b165dc86225c6f435dc1e03bb4ca49914
SilenceZhou <1063123603@qq.com>
2019-07-13 15:42:58 +0800

    53商品列表UI布局

--- 4_Project/flutter_shop/lib/pages/cart_page.dart
+++ 4_Project/flutter_shop/lib/pages/cart_page.dart
@@ -1,15 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop/models/cart_info_model.dart';
+import 'package:provide/provide.dart';
+import 'package:flutter_shop/provide/cart_provide.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 
-class CartPage extends StatefulWidget {
-  @override
-  _CartPageState createState() => _CartPageState();
-}
-
-class _CartPageState extends State<CartPage> {
+class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('购物车'),
+      ),
+      body: FutureBuilder(
+        future: _getCartInfo(context),
+        // initialData: InitialData,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData) {
+            List<CartInfoModel> cartModelList =
+                Provide.value<CartProvide>(context).cartInfoModelList;
+            return ListView.builder(
+              itemCount: cartModelList.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(cartModelList[index].goodsName),
+                );
+              },
+            );
+          } else {
+            return Text('正在加载....');
+          }
+        },
+      ),
+    );
+  }
+
+  Future<String> _getCartInfo(BuildContext context) async {
+    await Provide.value<CartProvide>(context).getCartInfo();
+    return 'end';
   }
 }
